f4e7ef149cb470a35e5e53a5f0d3ee7b4c57f738
魏孝宁 <weixn@neoceansoft.com>
2020-01-09 16:40:56 +0800

    修改修改标题栏方法

--- mylibrary/src/main/java/com/app/mylibrary/BaseActivity.java
+++ mylibrary/src/main/java/com/app/mylibrary/BaseActivity.java
@@ -169,7 +169,7 @@ public abstract class BaseActivity extends SwipeBackActivity {
         overridePendingTransition(0, R.anim.translate_dialog_out);
     }
 
-    void changeStatusBarTextColor(boolean isBlack) {
+   public void changeStatusBarTextColor(boolean isBlack) {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
             if (isBlack) {
                 getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//设置状态栏黑色字体
